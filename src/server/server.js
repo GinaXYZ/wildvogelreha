ef1af15976cc99cd6a02086d255b8bb36c4ab99e
GinaXYZ <qiiina122@hotmail.de>
2026-01-12 19:54:10 +0100

    Fix static images path for Docker deployment

--- src/server/server.js
+++ src/server/server.js
@@ -106,7 +106,8 @@ app.use(helmet({
 }));
 
 // Static file serving for images from public directory
-app.use('/api/images', express.static(path.join(__dirname, 'public')));
+// In Docker: public folder is mounted at /app/public
+app.use('/api/images', express.static(path.join(__dirname, '../../public')));
 
 // Rate Limiting - Allgemein
 const generalLimiter = rateLimit({
